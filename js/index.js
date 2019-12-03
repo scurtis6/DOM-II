d63c4d59759d7a6eea0f8ee1d5c7f09d1964fee5
Sierra Curtis <sierracurtis91@gmail.com>
2019-12-03 18:56:19 -0500

    added mouse and load events

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+// Your code goes here (mouseenter, mouseleave, mouseover, mouseout, load)
+const contentSection = document.querySelector(".content-section");
+contentSection.addEventListener("mouseenter", () => {
+    contentSection.style.transform = "scale(1.2)";
+    contentSection.style.transition = "transform 0.5s";
+})
+contentSection.addEventListener("mouseleave", () => {
+    contentSection.style.transform = "scale(1)";
+})
+
+const funBus = document.querySelector('h2');
+funBus.addEventListener('mouseover', function(){
+    funBus.style.color = 'red';
+})
+funBus.addEventListener('mouseout', () =>{
+    funBus.style.color = 'black'
+})
+
+// const textContent = document.querySelector(".intro");
+// textContent.addEventListener('scroll', () => {
+//     document.querySelector('p').innerHTML = "This this scrolling in div"
+// })
+// window.addEventListener('load', (event) =>{
+//     console.log('Page is fully loaded');
+// });
+window.addEventListener('load', function(e){
+    alert('This page has loaded');
+})
